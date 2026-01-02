a8f90fe952d7a1bd8d8f8dfd624fa55e27b01fb0
Yediael13 <yediaelratahinarivelo@gmail.com>
2026-01-03 00:54:20 +0100

    stash zonder svg

--- coredefender/src/main/java/be/iiw/coredefender/overlay/buildoverlay/BuildOverlayView.java
+++ coredefender/src/main/java/be/iiw/coredefender/overlay/buildoverlay/BuildOverlayView.java
@@ -21,17 +21,16 @@ public class BuildOverlayView {
     private final Button CanonButton;
     private final Button WallButton;
     private final Button GoldMineButton;
+    private final int buttonSize = 50;
 
     public BuildOverlayView() {
         overlayPane = new AnchorPane();
-        // Test of JavaFX je PNG kan vinden
-        URL test = getClass().getResource("/be/iiw/coredefender/Buildings_Images/GoldStash_Level_1.svg");
-        System.out.println("GoldStash svg gevonden: " + test);
+        
         
         int buttonTotal = 4; //aantal knoppen
-        int buttonSize = 50;
+        
         int spacing = 10;
-        int padding = 20;
+        int padding = 10;
         int width = buttonSize + 2 * padding;
         int totalHeight = padding * 2 + buttonTotal * buttonSize + (buttonTotal - 1) * spacing;
 
@@ -41,7 +40,7 @@ public class BuildOverlayView {
         container.setStroke(Color.web("#1B2640"));
         overlayPane.getChildren().add(container);
 
-        GoldStashButton = createButtonWithImage("/be/iiw/coredefender/Buildings_Images/GoldStash_Level_1.svg");
+        GoldStashButton = createButtonWithImage("/be/iiw/coredefender/Buildings_Images/GoldStash_Level_1.png");
         CanonButton = createButton("M5.5.5a.5.5 0 0 0-1 0V2A2.5 2.5 0 0 0 2 4.5H.5a.5.5 0 0 0 0 1H2v1H.5a.5.5 0 0 0 0 1H2v1H.5a.5.5 0 0 0 0 1H2v1H.5a.5.5 0 0 0 0 1H2A2.5 2.5 0 0 0 4.5 14v1.5a.5.5 0 0 0 1 0V14h1v1.5a.5.5 0 0 0 1 0V14h1v1.5a.5.5 0 0 0 1 0V14h1v1.5a.5.5 0 0 0 1 0V14a2.5 2.5 0 0 0 2.5-2.5h1.5a.5.5 0 0 0 0-1H14v-1h1.5a.5.5 0 0 0 0-1H14v-1h1.5a.5.5 0 0 0 0-1H14v-1h1.5a.5.5 0 0 0 0-1H14A2.5 2.5 0 0 0 11.5 2V.5a.5.5 0 0 0-1 0V2h-1V.5a.5.5 0 0 0-1 0V2h-1V.5a.5.5 0 0 0-1 0V2h-1zm1 4.5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3A1.5 1.5 0 0 1 6.5 5");
         WallButton = createButton("M6.707 9h4.364c-.536 1.573 2.028 3.806 4.929-.5-2.9-4.306-5.465-2.073-4.929-.5H6.707L4.854 6.146a.5.5 0 1 0-.708.708L5.293 8h-.586L2.854 6.146a.5.5 0 1 0-.708.708L3.293 8h-.586L.854 6.146a.5.5 0 1 0-.708.708L1.793 8.5.146 10.146a.5.5 0 0 0 .708.708L2.707 9h.586l-1.147 1.146a.5.5 0 0 0 .708.708L4.707 9h.586l-1.147 1.146a.5.5 0 0 0 .708.708z");
         GoldMineButton = createButton("Gold-mine.svg");
@@ -84,8 +83,8 @@ public class BuildOverlayView {
         Button btn = new Button();
         Image img = new Image(getClass().getResource(imagePath).toExternalForm());
         ImageView imgView = new ImageView(img);
-        imgView.setFitWidth(72);   // breedte van knop
-        imgView.setFitHeight(72);  // hoogte van knop
+        imgView.setFitWidth(buttonSize);   // breedte van knop
+        imgView.setFitHeight(buttonSize);  // hoogte van knop
         btn.setGraphic(imgView);
         btn.setFocusTraversable(false);
         return btn;
